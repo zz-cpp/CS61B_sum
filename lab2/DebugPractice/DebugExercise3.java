7e7e4c3ddd4342056d6aa8b28b491906758f5d22
root <root@DESKTOP-R6IFH5K.localdomain>
2022-03-22 23:07:02 +0800

    finish with recursion and iteration

---
+++ lab2/DebugPractice/DebugExercise3.java
@@ -0,0 +1,30 @@
+/**
+ * Created by jug on 1/22/18.
+ */
+public class DebugExercise3 {
+    public static int countTurnips(In in) {
+        int totalTurnips = 0;
+        while (!in.isEmpty()) {
+            String vendor = in.readString();
+            String foodType = in.readString();
+            double cost = in.readDouble();
+            int numAvailable = in.readInt();
+            if (foodType.equals("turnip")) {
+                // bug fix : clear a negative condition
+                if(numAvailable < 0){
+                    continue;
+                }
+
+                int newTotal = totalTurnips + numAvailable;
+                totalTurnips = newTotal;
+            }
+            in.readLine();
+        }
+        return totalTurnips;
+    }
+
+    public static void main(String[] args) {
+        In in = new In("foods.csv");
+        System.out.println(countTurnips(in));
+    }
+}
